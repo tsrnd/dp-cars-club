65d98eac6845fd2211803cc34645d1a2ba32e381
at-namnguyen <nam.nguyen@asiantech.vn>
2019-04-16 10:35:31 +0700

    auth middleware

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -2,6 +2,7 @@ import { validate, POST_SIGN_UP, POST_SIGN_IN } from './../util/validate';
 import * as express from 'express';
 import * as exampleController from '../http/controllers/example';
 import * as userController from '../http/controllers/user';
+import * as middleware from '../http/middleware/auth';
 
 const router = express.Router();
 
@@ -11,7 +12,7 @@ router.use((req: express.Request, res: express.Response, next: () => void) => {
 });
 
 // routes here
-router.get('/example', exampleController.index);
+router.get('/example', middleware.auth, exampleController.index);
 router.post('/signup', validate(POST_SIGN_UP), userController.signup);
 router.post('/signin', validate(POST_SIGN_IN), userController.signin);
 
